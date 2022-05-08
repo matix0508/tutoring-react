00de026cdfc86b9e0da4cd740befe4c5a60859e0
Mateusz Wlekliński <wleklinskimateusz05@gmail.com>
2022-05-08 19:06:14 +0200

    add types

---
+++ src/types/Lesson.ts
@@ -0,0 +1,5 @@
+export type Lesson = {
+    date: Date;
+    student_id: number;
+    cost: number;
+}
---
+++ src/types/Payment.ts
@@ -0,0 +1,5 @@
+export type Payment = {
+    date: Date;
+    student_id: number;
+    cost: number;
+}
